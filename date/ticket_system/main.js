0d6e0a3f66d28546333437fe4597157a9593a3be
rifat rahman <rifat15020154@gmail.com>
2021-12-10 22:32:11 +0600

    seat_name_jsn

--- date/ticket_system/main.js
+++ date/ticket_system/main.js
@@ -1,31 +1,34 @@
 var Coach_num;
-var fare_money,ID_number,mobile_number;
+var fare_money, ID_number, mobile_number;
 
+document.getElementById("view_port").addEventListener("click", function () {
+  document.getElementById("section_one").style.display = "none";
+  document.getElementById("section_two").style.display = "block";
+});
 
+document.getElementById("view_port_1").addEventListener("click", function () {
+  document.getElementById("section_one").style.display = "none";
+  document.getElementById("section_two").style.display = "block";
+});
+document.getElementById("view_port_2").addEventListener("click", function () {
+  document.getElementById("section_one").style.display = "none";
+  document.getElementById("section_two").style.display = "block";
+});
 
-document.getElementById("view_port").addEventListener("click",function(){
+function DataCatch() {
+  alert("hello");
 
-    document.getElementById("section_one").style.display="none";
-    document.getElementById("section_two").style.display="block"
-})
+  ID_number = document.getElementById("pin").value;
+  mobile_number = document.getElementById("num").value;
+  Coach_num = document.getElementById("seat_number").innerHTML.toString();
+  fare_money = document.getElementById("fare_money").innerHTML.toString();
 
+  var obj = {};
 
+  obj.DIU_ID = ID_number;
+  obj.Mobile = mobile_number;
+  obj.SeatNumber = Coach_num;
+  obj.Fare = fare_money;
 
-function DataCatch(){
-
-    alert("hello")
-
-    ID_number = document.getElementById("pin").value;
-    mobile_number = document.getElementById("num").value;
-    Coach_num = document.getElementById("seat_number").innerHTML.toString();
-    fare_money = document.getElementById("fare_money").innerHTML.toString();
-
-    var obj = {};
-
-    obj.DIU_ID = ID_number;
-    obj.Mobile = mobile_number;
-    obj.SeatNumber = Coach_num;
-    obj.Fare = fare_money;
-
-    console.log(obj);
+  console.log(obj);
 }
